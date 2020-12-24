f91e5e59f6ed82b37efe0dbcf9a098c15cbe3141
Juliemar Añonuevo <mr.juliemar.anonuevo@gmail.com>
2020-12-24 22:57:29 +0800

    Initial Commit

---
+++ try.py
@@ -0,0 +1,18 @@
+num = 4 + 5
+print(num, type(num))
+
+a, b, c, d = 34, 10.45, "Python Programming", True # int, float, stg, boolean or bool
+print("a =",a, "type =", type(a)) # int
+print("b =",b, "type =", type(b)) # float
+print("c =",c, "type =", type(c)) # string
+print("d =",d, "type =", type(d)) # boolean or bool
+
+
+age = input("Enter your age : ")
+print("Your age is: ",age)
+
+#expext an int
+age = int(input("Enter your age : "))
+print("Your age is: ",age)
+a = age * 5
+print(a)
